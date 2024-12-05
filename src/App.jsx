918613f1b15e65ab583f96fcb17e2955499a1430
Alexandre Dias <alexandredias027@gmail.com>
2024-12-04 22:49:36 -0300

    feat: Adicionado componente post

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 import styles from './App.module.css'
 import './global.css'
@@ -8,12 +10,11 @@ function App() {
   return (
     <div>
       <Header />
-      <h1>Teste</h1>
 
       <div className={styles.wrapper}>
-        <aside>Esse é o conteúdo do App</aside>
+        <Sidebar />
         <main>
-          <h1>Conteúdo Principal</h1>
+          <Post />
         </main>
       </div>
     </div>
